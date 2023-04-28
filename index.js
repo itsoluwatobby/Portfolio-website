f5dd01137a2aaa06e6dd78879b1d7d4c545ebe99
itsoluwatobby <oluwasamuel050@gmail.com>
2023-04-28 10:23:35 +0100

    done

--- index.js
+++ index.js
@@ -25,33 +25,41 @@ function closemenu() {
    sideMenuEl.style.right = '-200px';
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxzCuVxT2XUhsySgcb3pXGA5ZJeINUUNuyAEXNnKgwKZhVjRZ82zFpu5a6sO3J6uo91/exec'
+//const scriptURL = 'https://script.google.com/macros/s/AKfycbxzCuVxT2XUhsySgcb3pXGA5ZJeINUUNuyAEXNnKgwKZhVjRZ82zFpu5a6sO3J6uo91/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzMpHaXeqAnkOoJzmB8xtOO_pQCC3L3TRYcLU7VfcWe3PNc8ylFZWcNY5kitC8USIfQ/exec'
 const form = document.forms['submit-to-google-sheet']
 const msgEl = document.getElementById('msg')
 const msgErrorEl = document.getElementById('msgerr')
 const msgAlertEl = document.querySelector('.msg-alert')
+const buttonEl = document.querySelector('#button_form')
 
 form.addEventListener('submit', e => {
    e.preventDefault()
+   msgAlertEl.classList.remove('show')
+   msgEl.innerText = 'Submitting Message...'
+   buttonEl.style.backgroundColor = 'gray';
    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => {
+      msgEl.innerText = ''
       msgAlertEl.classList.remove('show')
       msgEl.innerText = 'Success!, Message sent successfully'
       setTimeout(() => {
          msgAlertEl.classList.add('show')
          msgEl.innerText = ''
       }, 3000);
-      
+      buttonEl.style.backgroundColor = '#ff004f'
       form.reset();
    })
       .catch(error => {
+      msgEl.innerText = ''
+      console.log(error)
       msgAlertEl.classList.remove('show')
       msgErrorEl.innerText = 'Error!, Unable to send message'
       setTimeout(() => {
          msgAlertEl.classList.add('show')
          msgErrorEl.innerText = ''
       }, 3000);
-      
+      buttonEl.style.backgroundColor = '#ff004f'
       form.reset();
    })
 })
